7b3b87dd9e9e4fe540f33b83692d9278b21f2ea7
aryan2712sp <aryansp2712@gmail.com>
2025-05-16 22:39:08 +0530

    Add files via upload

---
+++ Assignment_9/assignment4.c
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////
+//
+//  File name :     assignment4.c
+//  Description :   Write a program which accepts number from user and count frequency of 4 in it.
+//  Author :        Aryan S. Pardeshi.
+//  Date :          16/05/2025
+//
+/////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int CountFour(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    if (iNo < 0) // Handle negative input
+    {
+        iNo = -iNo;
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if (iDigit == 4)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int bRet = 0;
+
+    printf("Enter number :");
+    scanf("%d", &iValue);
+
+    bRet = CountFour(iValue);
+    printf("Frequency of 4 is :%d", bRet);
+    return 0;
+}
